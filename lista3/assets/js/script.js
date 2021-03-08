0eb4ddb37595669967d92a4d116ef8a0d0687ea4
Jecesario <jp@soloid.com.br>
2021-03-08 20:53:22 -0300

    fix: removendo variavel perida no código

--- lista3/assets/js/script.js
+++ lista3/assets/js/script.js
@@ -1,6 +1,5 @@
 // Script do exercício 1
 function copa() {
-    let anos = '';
     let text = document.querySelector('.text');
     text.innerHTML = 'Anos de copa: <br>';
     for(let i=1930; i<=2050; i=i+4) {
