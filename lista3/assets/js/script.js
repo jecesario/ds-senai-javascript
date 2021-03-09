10e39f792d62f9e8a6f3d5c2dceebd07a9963b02
Jecesario <jp@soloid.com.br>
2021-03-08 21:08:10 -0300

    feat: resolvendo exercicio 2 - contar entre os numeros digitados

--- lista3/assets/js/script.js
+++ lista3/assets/js/script.js
@@ -6,3 +6,22 @@ function copa() {
         text.innerHTML += i + '<br>';
     }
 }
+
+// Script do exercício 2
+function rodar() {
+    let n1 = document.querySelector('#n1').value;
+    let n2 = document.querySelector('#n2').value;
+    let resultado = document.querySelector('.resultado');
+    resultado.innerHTML = '';
+    if(n1 > n2) {
+        for(n2; n2<=n1; n2++) {
+            resultado.innerHTML += n2 + '<br>'; 
+        }
+    } else if (n2 > n1) {
+        for(n1; n1<=n2; n1++) {
+            resultado.innerHTML += n1 + '<br>'; 
+        }
+    } else {
+        resultado.innerHTML = n1;
+    }
+}
